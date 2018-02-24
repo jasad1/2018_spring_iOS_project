db396a8172c85da32d0d6fbadc047839b2de68bc
jasad1 <jasz.adam@hallgato.ppke.hu>
2018-02-24 14:29:13 +0100

    Add test single view app from practice

---
+++ 02/SingleViewApp/SingleViewApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Student on 2018. 02. 13..
+//  Copyright © 2018. Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorChangeButton: UIButton!
+    
+    var isDefaultColor = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func changeColorButtonPressed(_ sender: UIButton) {
+        if isDefaultColor {
+            sender.backgroundColor = UIColor.red
+            self.view.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.white
+            self.view.backgroundColor = UIColor.white
+        }
+        isDefaultColor = !isDefaultColor
+    }
+}
+
